c0dde14444fa9b8513d755abfd9483b62d3cbb8a
HIMANSHU6001 <hk9797592893@gmail.com>
2025-01-23 19:14:14 +0530

    inc torq dec impulse

--- components/Player.tsx
+++ components/Player.tsx
@@ -47,8 +47,8 @@ const Player = () => {
     const torque = { x: 0, y: 0, z: 0 };
 
     if (body.current) {
-      impulse.x = tilt.y * 0.2;
-      torque.z = -tilt.y * 0.2;
+      impulse.x = tilt.y * 0.1;
+      torque.z = -tilt.y * 0.4;
       body.current.applyImpulse(impulse);
     }
   }, [tilt]);
